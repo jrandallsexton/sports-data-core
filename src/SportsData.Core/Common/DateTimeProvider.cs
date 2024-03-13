80b655207eddcba41f3ca77eba0a811fe5f57a64
Randall Sexton <jrandallsexton@gmail.com>
2024-03-13 12:51:49 -0400

    minor additions

---
+++ src/SportsData.Core/Common/DateTimeProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SportsData.Core.Common
+{
+    /// <summary>
+    /// Appears something similar is now in .NET 8.  Review it: https://grantwinney.com/how-to-use-timeprovider-and-faketimeprovider/
+    /// </summary>
+    public interface IDateTimeProvider
+    {
+        DateTime UtcNow();
+    }
+
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/SportsData.Core/Exceptions/InvalidCommandException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SportsData.Core.Exceptions
+{
+    public class InvalidCommandException : Exception
+    {
+        public InvalidCommandException() { }
+
+        public InvalidCommandException(string message) :
+            base(message)
+        { }
+    }
+}

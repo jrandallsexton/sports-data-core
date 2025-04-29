44c854f55ea98d36dcca6b8e33a2a55c66c8eb19
Randall Sexton <jrandallsexton@gmail.com>
2025-04-29 05:11:15 -0400

    lint issue

--- src/UI/sd-ui/src/api/apiClient.js
+++ src/UI/sd-ui/src/api/apiClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import toast from "react-hot-toast";
+//import toast from "react-hot-toast";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:3001", // 🔥 later swap to your real server

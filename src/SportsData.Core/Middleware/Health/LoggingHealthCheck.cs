d43bf5454988680ddd68157a7ceabcff8bc40152
Randall Sexton <jrandallsexton@gmail.com>
2025-05-14 07:29:22 -0400

    fix logging HC

--- src/SportsData.Core/Middleware/Health/LoggingHealthCheck.cs
+++ src/SportsData.Core/Middleware/Health/LoggingHealthCheck.cs
@@ -22,12 +22,7 @@ namespace SportsData.Core.Middleware.Health
             HealthCheckContext context,
             CancellationToken cancellationToken = new CancellationToken())
         {
-            //var response = await new HttpClient().GetAsync("http://seq-ui/#/events?range=1d", cancellationToken);
-            //var response = await new HttpClient().GetAsync("http://localhost:8090/#/events?range=1d", cancellationToken);
-
-            //var response = await new HttpClient().GetAsync(_config.Value.SeqUri, cancellationToken);
-            // TODO: Move this updated value to AzAppConfig
-            var response = await new HttpClient().GetAsync("http://localhost:8090/health", cancellationToken);
+            var response = await new HttpClient().GetAsync(_config.Value.SeqUri, cancellationToken);
 
             return response.IsSuccessStatusCode ?
                 HealthCheckResult.Healthy() :

6dd3dd6f5e48c861f44b14b8b12f12be8a3b2028
Randall Sexton <jrandallsexton@gmail.com>
2025-05-26 09:15:01 -0400

    venues in api

--- src/SportsData.Core/Infrastructure/Clients/Venue/DTOs/VenueDto.cs
+++ src/SportsData.Core/Infrastructure/Clients/Venue/DTOs/VenueDto.cs
@@ -1,15 +1,21 @@
-﻿namespace SportsData.Core.Infrastructure.Clients.Venue.DTOs
+﻿using SportsData.Core.Dtos.Canonical;
+
+using System.Collections.Generic;
+
+namespace SportsData.Core.Infrastructure.Clients.Venue.DTOs
 {
     public record VenueDto
     {
-        public int Id { get; set; }
+        public required string Name { get; set; }
 
-        public string Name { get; set; }
-
-        public string ShortName { get; set; }
+        public required string ShortName { get; set; }
 
         public bool IsGrass { get; set; }
 
         public bool IsIndoor { get; set; }
+
+        public required string Slug { get; set; }
+
+        public List<VenueImageDto> Images { get; set; } = new List<VenueImageDto>();
     }
 }

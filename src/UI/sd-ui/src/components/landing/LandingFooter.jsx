d512c55766e1ecfae052f22b23eafb5a6ab2f68d
Randall Sexton <jrandallsexton@gmail.com>
2025-11-18 17:39:46 -0500

    bumpity bump

--- src/UI/sd-ui/src/components/landing/LandingFooter.jsx
+++ src/UI/sd-ui/src/components/landing/LandingFooter.jsx
@@ -10,7 +10,7 @@ function LandingFooter() {
           <span className="footer-brand">
             sportDeets<span className="tm-symbol">™</span>
           </span>
-          . All rights reserved. v1811251729
+          . All rights reserved. v1811251739
         </p>
 
         <div className="footer-links">

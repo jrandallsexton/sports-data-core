b75d3dd2e31aac996c38aa87f1e5f878e468cfc5
Randall Sexton <jrandallsexton@gmail.com>
2025-12-06 14:48:01 -0500

    logo url updates

--- sql/pgsql/cleanup_franchiseSeasonLogos.sql
+++ sql/pgsql/cleanup_franchiseSeasonLogos.sql
@@ -1,26 +1,26 @@
 -- Check how many rows will be affected
 SELECT COUNT(*) as rows_to_update
 FROM public."FranchiseSeasonLogo"
-WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-franchiselogo%';
+WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-teambyseasonlogo-%';
 
 -- Preview the changes that will be made
 SELECT 
     "Id",
     "Uri" as old_uri,
     REPLACE("Uri", 
-        'sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-franchiselogo',
-        'sportdeetssa.blob.core.windows.net/franchise-logo-football-ncaa'
+        'sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-teambyseasonlogo-',
+        'sportdeetssa.blob.core.windows.net/team-by-season-logo-football-ncaa-'
     ) as new_uri
 FROM public."FranchiseSeasonLogo"
-WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-franchiselogo%';
+WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-teambyseasonlogo-%';
 
 -- Update the URIs to point to production storage account
 UPDATE public."FranchiseSeasonLogo"
 SET "Uri" = REPLACE("Uri", 
-    'sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-franchiselogo',
-    'sportdeetssa.blob.core.windows.net/franchise-logo-football-ncaa'
+    'sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-teambyseasonlogo-',
+    'sportdeetssa.blob.core.windows.net/team-by-season-logo-football-ncaa-'
 )
-WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-franchiselogo%';
+WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-teambyseasonlogo-%';
 
 -- Verify the update
 SELECT "Id", "Uri" 

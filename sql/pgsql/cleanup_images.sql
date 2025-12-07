647481b986e8348bf885451c767c25c8c84ac669
Randall Sexton <jrandallsexton@gmail.com>
2025-12-07 08:50:35 -0500

    more img cleanup

--- sql/pgsql/cleanup_images.sql
+++ sql/pgsql/cleanup_images.sql
@@ -1,26 +1,26 @@
 -- Check how many rows will be affected
 SELECT COUNT(*) as rows_to_update
 FROM public."AthleteImage"
-WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-athleteimage-%';
+WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/prod-provider-footballncaa-athleteimage%';
 
 -- Preview the changes that will be made
 SELECT 
     "Id",
     "Uri" as old_uri,
     REPLACE("Uri", 
-        'sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-athleteimage-',
-        'sportdeetssa.blob.core.windows.net/athlete-image-football-ncaa-'
+        'sportdeetssadev.blob.core.windows.net/prod-provider-footballncaa-athleteimage',
+        'sportdeetssa.blob.core.windows.net/athlete-image-football-ncaa'
     ) as new_uri
 FROM public."AthleteImage"
-WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-athleteimage-%';
+WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/prod-provider-footballncaa-athleteimage%';
 
 -- Update the URIs to point to production storage account
 UPDATE public."AthleteImage"
 SET "Uri" = REPLACE("Uri", 
-    'sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-athleteimage-',
-    'sportdeetssa.blob.core.windows.net/athlete-image-football-ncaa-'
+    'sportdeetssadev.blob.core.windows.net/prod-provider-footballncaa-athleteimage',
+    'sportdeetssa.blob.core.windows.net/athlete-image-football-ncaa'
 )
-WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/dev-provider-footballncaa-athleteimage-%';
+WHERE "Uri" LIKE '%sportdeetssadev.blob.core.windows.net/prod-provider-footballncaa-athleteimage%';
 
 -- Verify the update
 SELECT "Id", "Uri" 

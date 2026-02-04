09e4b2cf41bc8f764ddbd70f5293c885605012d6
Randall Sexton <jrandallsexton@gmail.com>
2026-02-04 05:01:56 -0500

    forgot migration push

--- src/SportsData.Producer/Migrations/Football/20260203234117_03FebV1_AthSeasonInj.Designer.cs
+++ src/SportsData.Producer/Migrations/Football/20260203234117_03FebV1_AthSeasonInj.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SportsData.Producer.Infrastructure.Data.Football;
@@ -12,9 +13,11 @@ using SportsData.Producer.Infrastructure.Data.Football;
 namespace SportsData.Producer.Migrations.Football
 {
     [DbContext(typeof(FootballDataContext))]
-    partial class FootballDataContextModelSnapshot : ModelSnapshot
+    [Migration("20260203234117_03FebV1_AthSeasonInj")]
+    partial class _03FebV1_AthSeasonInj
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/SportsData.Producer/Migrations/Football/20260203234117_03FebV1_AthSeasonInj.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsData.Producer.Migrations.Football
+{
+    /// <inheritdoc />
+    public partial class _03FebV1_AthSeasonInj : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AthleteSeasonInjury",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AthleteSeasonId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TypeId = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    TypeDescription = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    TypeAbbreviation = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Headline = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    Text = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    Source = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false),
+                    CreatedUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AthleteSeasonInjury", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AthleteSeasonInjury_AthleteSeason_AthleteSeasonId",
+                        column: x => x.AthleteSeasonId,
+                        principalTable: "AthleteSeason",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AthleteSeasonNotes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AthleteSeasonId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Headline = table.Column<string>(type: "text", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    Source = table.Column<string>(type: "text", nullable: true),
+                    RowVersion = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AthleteSeasonNotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AthleteSeasonNotes_AthleteSeason_AthleteSeasonId",
+                        column: x => x.AthleteSeasonId,
+                        principalTable: "AthleteSeason",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AthleteSeasonInjury_AthleteSeasonId",
+                table: "AthleteSeasonInjury",
+                column: "AthleteSeasonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AthleteSeasonNotes_AthleteSeasonId",
+                table: "AthleteSeasonNotes",
+                column: "AthleteSeasonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AthleteSeasonInjury");
+
+            migrationBuilder.DropTable(
+                name: "AthleteSeasonNotes");
+        }
+    }
+}

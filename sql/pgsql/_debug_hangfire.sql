da1ec7389fe2a25e5dbeae20fb5ee5bd3f900303
Randall Sexton <jrandallsexton@gmail.com>
2026-02-25 07:22:25 -0500

    Feature/row count observability (#112)
    * docs and misc
    
    * sourcing saga changes

--- sql/pgsql/_debug_hangfire.sql
+++ sql/pgsql/_debug_hangfire.sql
@@ -13,10 +13,15 @@ GROUP BY DATE(createdat)
 ORDER BY date DESC 
 LIMIT 10;
 
-  select count(*) from hangfire."job"
+  select count(*) from hangfire."job" where "statename" = 'Enqueued';
   select count(*) from hangfire."state"
   SELECT * FROM hangfire."lock";
 
+  SELECT COUNT(*)::int FROM hangfire.jobqueue WHERE fetchedat IS NULL
+
+SELECT * FROM hangfire.jobqueue
+
+select * from hangfire."job" limit 10;
   SELECT COUNT(*) as count 
 FROM hangfire.job 
 ORDER BY count DESC;
